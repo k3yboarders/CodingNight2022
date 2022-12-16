ed648dafa09d73403b66e7346b2073bb4117220d
Piksel654 <marcingorowski333@gmail.com>
2022-12-16 22:07:00 +0100

    phone number nullable in Publisher class

---
+++ Migrations/20221216210547_phonenumber.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LibraryCodingNight.Migrations
+{
+    public partial class phonenumber : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "Publisher",
+                type: "varchar(20)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(20)")
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Publisher",
+                keyColumn: "PhoneNumber",
+                keyValue: null,
+                column: "PhoneNumber",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "PhoneNumber",
+                table: "Publisher",
+                type: "varchar(20)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "varchar(20)",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+        }
+    }
+}
