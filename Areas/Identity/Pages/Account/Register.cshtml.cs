1331c7d56a84f0f2e0daa3d0cdddfb3fd1a380d3
seweryn <seweryn@codingmonster.codingmonster.com>
2022-12-16 20:11:29 +0100

    Register and Login views update

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -79,6 +79,19 @@ namespace LibraryCodingNight.Areas.Identity.Pages.Account
             [Display(Name = "Email")]
             public string Email { get; set; }
 
+	    [Required]
+            [Display(Name = "Imię")]
+            public string FirstName { get; set; }
+
+            [Required]
+            [Display(Name = "Nazwisko")]
+            public string LastName { get; set; }
+
+            [Required]
+            [Display(Name = "Data urodzenia")]
+            [DataType(DataType.Date)]
+            public DateOnly BirthDate { get; set; }
+
             /// <summary>
             ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
             ///     directly from your code. This API may change or be removed in future releases.
