9d7ce86151741914ef210d374b22bd754b4d0e57
Maksymilian Gala <mgwspinanie2006@wp.pl>
2022-12-16 18:16:12 +0100

    init

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LibraryCodingNight.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
