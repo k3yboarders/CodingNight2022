e18ee0db2d68964fe78307a89764530c054b8f14
seweryn <seweryn@codingmonster.codingmonster.com>
2022-12-16 21:15:56 +0100

    Fix UI links in Dashboard

--- Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
+++ Areas/Identity/Pages/Account/Manage/ManageNavPages.cs
@@ -61,6 +61,7 @@ namespace  LibraryCodingNight.Areas.Identity.Pages.Account.Manage
         /// </summary>
         public static string TwoFactorAuthentication => "TwoFactorAuthentication";
 
+        public static string Profile => "Profile";
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
@@ -109,6 +110,7 @@ namespace  LibraryCodingNight.Areas.Identity.Pages.Account.Manage
         /// </summary>
         public static string TwoFactorAuthenticationNavClass(ViewContext viewContext) => PageNavClass(viewContext, TwoFactorAuthentication);
 
+        public static string ProfileNavClass(ViewContext viewContext) => PageNavClass(viewContext, Profile);
         /// <summary>
         ///     This API supports the ASP.NET Core Identity default UI infrastructure and is not intended to be used
         ///     directly from your code. This API may change or be removed in future releases.
